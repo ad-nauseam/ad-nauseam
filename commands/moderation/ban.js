09fed3a4450cbfa0c7bfadb569aa7303fe04f97d
itsUrcute <riniefrinoo@gmail.com>
2021-03-10 10:14:11 +0530

    polished ban

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -11,20 +11,27 @@ class BanCommand extends Command {
                 {
                     id: 'reason',
                     match : 'rest'
+                },
+                {
+                    id: 'days',
+                    match: 'option',
+                    flag: '--d',
+                    type: 'integer',
+                    default: 0
                 }
            ]
         });
     }
 
     async exec(message,args) {
-        let toBan = message.mentions.members.first() || args.id;
-        let clientMember = message.guild.members.cache.get(message.client.user.id);
+        let toBan = message.mentions.users.first() || await message.client.users.fetch(args.id).catch( err => {} );
+        let clientMember = message.guild.me;
         
         if (message.member.roles.highest.position <= clientMember.roles.highest.position) return;
 
-        message.guild.members.ban(toBan, {reason : args.reason })
+        message.guild.members.ban(toBan, {reason : args.reason, days : args.days })
         .then(banned => { 
-            message.channel.send(`Banned ${banned.user.tag}`)
+            message.channel.send(`Banned **${toBan.tag}**`)
             /*
             Logging + DB Stuff here
             */
