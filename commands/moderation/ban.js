0f94442bc43ff73adb0a1c5f4a8b3780355c08c3
itsUrcute <riniefrinoo@gmail.com>
2021-03-10 10:27:59 +0530

    code polish

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -24,20 +24,20 @@ class BanCommand extends Command {
     }
 
     async exec(message,args) {
-        let toBan = message.mentions.users.first() || await message.client.users.fetch(args.id).catch( err => {} );
+        let toBan = message.mentions.users.first() || await message.client.users.fetch(args.id).catch( () => {} );
         let clientMember = message.guild.me;
         
         if (message.member.roles.highest.position <= clientMember.roles.highest.position) return;
 
         message.guild.members.ban(toBan, {reason : args.reason, days : args.days })
-        .then(banned => { 
+        .then( () => { 
             message.reply(`Banned **${toBan.tag}**`)
             /*
             Logging + DB Stuff here
             */
     
         })
-        .catch(err => message.reply('Cannot ban this user'));
+        .catch( () => message.reply('Cannot ban this user'));
         
     }
 }
