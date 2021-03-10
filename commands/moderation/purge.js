f5004e8d5e487ce6d219810ac203b9cce52f1c31
itsUrcute <riniefrinoo@gmail.com>
2021-03-10 13:56:59 +0530

    clear command and utils file

---
+++ commands/moderation/purge.js
@@ -0,0 +1,29 @@
+const { Command } = require('discord-akairo');
+const utils = require("./../../utils.js")
+
+class PurgeCommand extends Command {
+    constructor() {
+        super('purge', {
+           aliases: ['purge', 'clear'],
+           args: [
+                {
+                    id: 'amt',
+                    match: 'content',
+                    type: 'integer'
+                }
+           ]
+        });
+    }
+
+    exec(message,args) {
+        if (!utils.isStaff(message.member) || (args.amt < 1 || args.amt > 100)) return;
+        message.channel.bulkDelete(args.amt)
+        .then(messages => {
+            message.channel.send(`Successfully cleared ${messages.size} messages`)
+            .then(m => m.delete({timeout : 1000}));
+        })
+        .catch((err) => {console.log(err)});
+    }
+}
+
+module.exports = PurgeCommand;
---
+++ utils.js
@@ -0,0 +1,17 @@
+module.exports = {
+    isStaff : function(guildMember){
+        if(guildMember.roles.highest >= guildMember.guild.roles.cache.find(r => r.name.toLowerCase() == "staff").position){
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    isMod : function(guildMember){
+        if(guildMember.roles.highest >= guildMember.guild.roles.cache.find(r => r.name.toLowerCase() == "moderator").position){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
