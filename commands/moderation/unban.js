f84d692d2e5d3410d0e0ebc8867b253411ec6e09
itsUrcute <riniefrinoo@gmail.com>
2021-05-17 14:47:47 +0530

    Improved string matching in unban

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -1,5 +1,5 @@
 const { Command } = require('discord-akairo');
-const { dice } = require('./../../utils/dice.js')
+const { unbanDice } = require('./../../utils/dice.js')
 class UnbanCommand extends Command {
     constructor() {
         super('unban', {
@@ -21,7 +21,7 @@ class UnbanCommand extends Command {
         if(!to){
             let list = await message.guild.fetchBans()
             if (!list.size) return message.reply("there are no banned members")
-            list = list.map(x => [x.user, dice(args.id, x.user.tag)]).sort((a,b) => b[1] - a[1])
+            list = unbanDice(list.map(x => x.user), args.id).sort((a,b) => b[1] - a[1])
             if ( list[0][1] > 0.6 ) to = list[0][0]
             else{
                 return message.reply(`no close matches, are you looking for\`\`\`\n${list.slice(0,6).map(x => x[0].tag).join("\n")}\`\`\``)
--- utils/dice.js
+++ utils/dice.js
@@ -1,36 +1,72 @@
-function dice(stra, strb){
-    stra.replace(/\s+/g, "").toLowerCase();
-    strb.replace(/\s+/g, "").toLowerCase();
-    if (stra === strb) return 1;
-    else if (stra.length < 2 || strb.length < 2) return 0;
-    
-   function bigrams(str){
-     let strbi = new Map();
-     for(let i = 0; i < str.length - 1; i++){
-       let bi = str.substring(i,i+2);
-       if (strbi.has(bi)){
-         let a = strbi.get(bi);
-         strbi.set(bi, a + 1);
-       } else {
-         strbi.set(bi, 1);
-       }
-     }
-     return strbi;
-   }
-   
-   let abi = bigrams(stra);
-   
-   function common(abi, strb){
-     let count = 0;
-     for (let i = 0; i < strb.length - 1; i++ ){
-       let bi = strb.substring(i,i+2);
-       if ( abi.has(bi) ) count++;
-     }
-     return count;
-   }
-   let asize = stra.length - 1;
-   let bsize = strb.length - 1;
-   return 2 * common(abi, strb) / (asize + bsize);
+function bigrams(str) {
+  let strbi = new Map();
+  for (let i = 0; i < str.length - 1; i++) {
+    let bi = str.substring(i, i + 2);
+    if (strbi.has(bi)) {
+      let a = strbi.get(bi);
+      strbi.set(bi, a + 1);
+    } else {
+      strbi.set(bi, 1);
+    }
   }
- 
- module.exports.dice = dice
+  return strbi;
+}
+
+function common(abi, strb) {
+  let count = 0;
+  for (let i = 0; i < strb.length - 1; i++) {
+    let bi = strb.substring(i, i + 2);
+    if (abi.has(bi)) count++;
+  }
+  return count;
+}
+
+
+
+function dice(stra, strb) {
+  stra = stra.replace(/\s+/g, "").toLowerCase();
+  strb = strb.replace(/\s+/g, "").toLowerCase();
+
+  if (stra === strb) return 1;
+  else if (stra.length < 2 || strb.length < 2) return 0;
+
+  let abi = bigrams(stra);
+
+  let asize = stra.length - 1;
+  let bsize = strb.length - 1;
+
+  return 2 * common(abi, strb) / (asize + bsize);
+}
+
+
+function arrayDice(arr, str) {
+  newarr = [...arr].map(x => x.replace(/\s+/g, "").toLowerCase());
+  str = str.replace(/\s+/g, "").toLowerCase();
+  if (str.length < 2) return new Array(arr.length).fill(0);
+
+  let abi = bigrams(str);
+
+  return newarr.map(x => {
+    if (str === x) return 1;
+    else if (x.length < 2) return 0;
+    return 2 * common(abi, x) / (x.length + str.length - 2);
+  });
+}
+
+
+function unbanDice(unblist, str){
+  if (str.length < 2) return new Array(unblist.length).fill(0);
+
+  let abi = bigrams(str.replace(/\s+/g, "").toLowerCase());
+
+  return unblist.map(x => {
+    if (str === x.tag) return [x, 1];
+    else if (x.tag.length < 2) return [x, 0];
+    return [x, 2 * common(abi, x.tag.replace(/\s+/g, "").toLowerCase()) / (x.tag.length + str.length - 2)];
+  });
+
+}
+
+module.exports.dice = dice;
+module.exports.arrayDice = arrayDice;
+module.exports.unbanDice = unbanDice;
